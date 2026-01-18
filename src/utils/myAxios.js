c94d3e7c4277b6c0c9001fd108f94554410e5608
bhaavansai <133352380+bhaavansai@users.noreply.github.com>
2026-01-18 22:37:46 +0530

    Update myAxios.js

--- src/utils/myAxios.js
+++ src/utils/myAxios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // import { useLoading } from "../LoadingContext";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://backend-donordash.onrender.com/",
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://backend-donordash.onrender.com/",
+  // baseURL: "http://localhost:8080/",
 });
 
 export const setupInterceptors = (showLoader, hideLoader) => {
@@ -55,26 +55,5 @@ export const setupInterceptors = (showLoader, hideLoader) => {
     axiosInstance.interceptors.response.eject(resId);
   };
 };
-
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     showLoader();
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     hideLoader(); // Hide loader on success
-//     return response;
-//   },
-//   (error) => {
-//     hideLoader(); // Hide loader on error
-//     return Promise.reject(error);
-//   }
-// );
 
 export default axiosInstance;
